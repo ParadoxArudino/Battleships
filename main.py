2d634ce688c913be11d73c06ce491f56f05ab19f
ParadoxArudino <danielmb062020@gmail.com>
2023-12-31 12:15:36 +0000

    Added frameCount and runtime variables

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ from collections import deque
 from simple_objects import *
 from game_objects import *
 from menu_objects import *
+from utils import *
 
 class Engine:
     def __init__(self, maxFPS):
         
         #init colours
         self.background_colour = (0, 0, 0)
-
+        
         #init window
-        self.surface = pygame.display.set_mode((1280, 920))
+        self.surface = pygame.display.set_mode((1280, 920), pygame.RESIZABLE)
         pygame.display.set_caption('Battleships')
 
-        #init components
+        #init runtime variables
         self.maxFPS = maxFPS #Max FPS 
         self.clock = pygame.time.Clock() #Clock instance
+        self.startTime = time.time() #Start time
+        self.runTime = 0 #Run time
+        self.frameCount = 0 #Frame count
         self.exited = False #Exit flag
         self.recent_frame_times = deque(maxlen=10) #Queue of frame times
+
+        #init objects
         self.mode = 0 #0 = menu, 1 = game, 2 = card game
         self.objectsG: list[GameObject] = [] #List of mage gamemode game objects
         self.objectsM: list[MenuObject] = [] #List of menu objects
@@ -69,6 +75,10 @@ class Engine:
         #Updates state and position of all menu objects. Called once per frame
         for event in pygame.event.get():
             self.on_event(event)
+
+        if self.frameCount % 60 == 0: #Every second, update the background colour to a random colour
+            self.background_colour = RandColour()
+            print("FPS: " + str(self.clock.get_fps()))
     
     #def execute_cardtick(self):
     #    #Updates state and position of all card game objects. Called once per frame
@@ -93,23 +103,25 @@ class Engine:
 
         pygame.display.flip()
 
-        def run(self):
-            #Main loop
-            while not self.exited:
-                self.clock.tick(self.maxFPS)
-                self.recent_frame_times.append(self.clock.get_time())
+    def run(self):
+        #Main loop
+        while not self.exited:
+            self.clock.tick(self.maxFPS)
+            self.recent_frame_times.append(self.clock.get_time())
+            self.runTime = time.time() - self.startTime
+            self.frameCount += 1
 
-                if self.mode == 0:
-                    self.execute_menutick()
-                elif self.mode == 1:
-                    self.execute_gametick()
-                #elif self.mode == 2:
-                #    self.execute_cardtick()
-                else:
-                    print("Invalid mode")
-                self.execute_render()
-            
-            pygame.quit()
+            if self.mode == 0:
+                self.execute_menutick()
+            elif self.mode == 1:
+                self.execute_gametick()
+            #elif self.mode == 2:
+            #    self.execute_cardtick()
+            else:
+                print("Invalid mode")
+            self.execute_render()
+        
+        pygame.quit()
     
 instance = Engine(60)
 instance.run() 
